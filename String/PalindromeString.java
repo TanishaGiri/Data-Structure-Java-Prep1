d8124f2dcb54aebdb7ce1b079d7d4142e6ebff70
TanishaGiri <tanisha.giri2507@gmail.com>
2023-07-02 12:38:36 +0530

    Committing PalindromeString

---
+++ String/PalindromeString.java
@@ -0,0 +1,15 @@
+public class PalindromeString{
+    public static boolean palindrome(String str){
+    for(int i=0; i<str.length()/2; i++){
+    if(str.charAt(i) != str.charAt(str.length()-i-1)){
+    return false;
+    }
+    }
+    return true;
+    }
+    public static void main(String[] args){
+    String str = "ABBA";
+    PalindromeString obj = new PalindromeString();
+    System.out.println(obj.palindrome(str));
+    }
+}
